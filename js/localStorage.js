cbade5b4636b9af96ff73748f48cc6911c98f60a
Matheus J. Vieira Lemes <matheus.vieira@viamondo.com.br>
2016-06-03 17:18:31 -0300

    Modificações LocalStorage

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,10 +1,24 @@
-function salvar() {
-      
-    if(title == ""){
-          alert("Dados incorretos, tente novamente");
-    }else{
-        localStorage.setItem("title", $("#title").val());
-        alert("Dados salvos");
-    }    
-}
+$(function(){
+    if(tbMovies == null) // Caso não haja conteúdo, iniciamos um vetor vazio
+    tbMovies = [];
+    tbMovies = JSON.parse(tbMovies); // Converte string para objeto
+    var operacao = "A"; //"A"=Adição; "E"=Edição
+    var indice_selecionado = -1; //Índice do item selecionado na lista
+    var tbMovies = localStorage.getItem("tbMovies");// Recupera os dados armazenados
+    
+});
 
+function salvar(){
+    console.log("teste")
+    var movie = JSON.stringify({
+        Title  : $("#title").val(),
+        Genre     : $("#genre").val(),
+        Language : $("#languade").val(),
+        Year    : $("#year").val(),
+        Sinopse : $()
+    });
+    tbMovies.push(movie);
+    localStorage.setItem("tbMovies", JSON.stringify(tbMovies));
+    alert("Registro adicionado.");
+    return true;
+}
