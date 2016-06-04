55198df4f477333fa85f698a038074068c808a4a
Matheus José <matheus_lemes1996@hotmail.com>
2016-06-03 21:37:14 -0300

    Configurção LocalStorage e RegistrationScren

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,24 +1,17 @@
-$(function(){
-    if(tbMovies == null) // Caso não haja conteúdo, iniciamos um vetor vazio
-    tbMovies = [];
-    tbMovies = JSON.parse(tbMovies); // Converte string para objeto
-    var operacao = "A"; //"A"=Adição; "E"=Edição
-    var indice_selecionado = -1; //Índice do item selecionado na lista
-    var tbMovies = localStorage.getItem("tbMovies");// Recupera os dados armazenados
+function salvar() {
+    var movie = {
+        title: $('#title').val(),
+        genre: $('#genre').val(),
+        language: $('#language').val(),
+        year: $('#year').val(),
+        sinopse: $('#sinopse').val()
+    };
     
-});
-
-function salvar(){
-    console.log("teste")
-    var movie = JSON.stringify({
-        Title  : $("#title").val(),
-        Genre     : $("#genre").val(),
-        Language : $("#languade").val(),
-        Year    : $("#year").val(),
-        Sinopse : $()
-    });
-    tbMovies.push(movie);
-    localStorage.setItem("tbMovies", JSON.stringify(tbMovies));
-    alert("Registro adicionado.");
-    return true;
+    if(movie.title != "" && movie.language != "" && movie.year != "" && movie.sinopse != ""){
+        window.localStorage.setItem('' + movie.title, JSON.stringify(movie));
+        alert("Dados Salvos");        
+    }else{
+        alert("Preencha todos campos!");
+    }
 }
+
