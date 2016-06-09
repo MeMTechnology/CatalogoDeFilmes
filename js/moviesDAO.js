f7c2dba0451b9a0c0bb582b7614d009168cefb2f
Matheus José <matheus_lemes1996@hotmail.com>
2016-06-09 07:57:29 -0300

    Adição de pesquisa

--- js/moviesDAO.js
+++ js/moviesDAO.js
@@ -25,14 +25,41 @@ var moviesDao = {
     },
     
     getMoviesById: function(search) {
-        moviesDao.loadMovies();
         return moviesDao.movies.filter(function(a) { 
-            return a.title.includes(search) || a.year == search || a.language == search || a.genre == search
+            return a.title.includes(search) || a.year == search || a.language == search || a.genre_movie == search
+        });
+    },
+    
+    getActionSearch: function() {
+        $('#search').on('click', function() {
+           $('#tbMovies tbody').html('');
+           var buscado = $("[name=q]").val();
+            var ls = window.localStorage.getItem('list-movies');
+            moviesDao.getMoviesById(buscado).forEach(function(resultado) {
+                if(moviesDao.getMoviesById(buscado).length != 0) {
+                    $('#tbMovies tbody').append('<tr>');
+                    $('#tbMovies tbody tr:last').append('<td>'+resultado.title+'</td>');
+                    $('#tbMovies tbody tr:last').append('<td>'+resultado.genre+'</td>');
+                    $('#tbMovies tbody tr:last').append('<td>'+resultado.language+'</td>');
+                    $('#tbMovies tbody tr:last').append('<td>'+resultado.year+'</td>');
+                    $('#tbMovies tbody tr:last').append('<td>'+resultado.sinopse+'</td>');
+                    $('#tbMovies tbody').append('</tr>');
+                }
+            });
+            $("[name=q]").val('');
         });
     },
     
     listResults: function(name){
+        moviesDao.loadMovies();
         var list = moviesDao.getMoviesById(name);
         
-    }  
+    },
+    
+    createTrsElements: function(e){
+        console.log(e.title);
+    }
 };
+
+moviesDao.loadMovies();
+moviesDao.getActionSearch();
